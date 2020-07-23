e86d6872cdea91dc6d286f75c11785d36e1a047c
Douglas Dunn <douglas.dunn@outlook.com>
2020-07-22 22:05:55 -0700

    created HelloExpressions component

---
+++ force-app/main/default/lwc/helloExpressions/helloExpressions.js
@@ -0,0 +1,18 @@
+import { LightningElement } from 'lwc';
+
+export default class HelloExpressions extends LightningElement {
+    firstName = '';
+    lastName = '';
+
+    handleChange(event) {
+        if (event.target.name === 'firstName') {
+            this.firstName = event.target.value;
+        } else if (event.target.name === 'lastName') {
+            this.lastName = event.target.value;
+        }
+    }
+
+    get uppercasedFullname() {
+        return `${this.firstName} ${this.lastName}`.trim().toUpperCase();
+    }
+}
